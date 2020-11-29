37d76960b53b22409200eec1056a518f26d42a49
HenryTheGhostBuster <74766884+HenryTheGhostBuster@users.noreply.github.com>
2020-11-29 16:19:40 +0200

    Final xd

--- js/script.js
+++ js/script.js
@@ -3,29 +3,24 @@ const gui = {
     currentScore: document.querySelector('#scorecrnt'),
     personalBestScore: document.querySelector('#scorepb'),
     button: document.querySelector('button'),
-    gameScreen: document.querySelector('canvas')
+    gameScreen: document.querySelector('canvas'),
+    overlay: document.querySelector('.overlay')
 };
 
 const gameScreenContext = gui.gameScreen.getContext('2d');
 
 class Wall {
-    constructor(width, height, speed) {
+    constructor(width, height) {
         this.x = 0;
         this.y = 0;
         this.width = width;
         this.height = height;
-        this.speed = speed;
-        this.thickness = 20;
-    }
-
-    update(deltaTime, thickness) {
-        if (this.thickness < thickness)
-            this.thickness += deltaTime * this.speed;
+        this.thickness = 10;
     }
 
     draw() {
         gameScreenContext.beginPath();
-        gameScreenContext.lineWidth = String(this.thickness);
+        gameScreenContext.lineWidth = this.thickness;
         gameScreenContext.strokeStyle = "#000";
         gameScreenContext.rect(this.x, this.y, this.width, this.height);
         gameScreenContext.stroke();
@@ -57,6 +52,25 @@ class Token {
         gameScreenContext.fillStyle = '#f1c40f';
         gameScreenContext.fillRect(this.x, this.y, this.width, this.height);
         gameScreenContext.fillStyle = '#000';
+    }
+
+    spawnRandom(gameObjects) {
+
+        // dejau
+        let x = 0;
+        let y = 0;
+
+        x = Math.floor(Math.random() * gui.gameScreen.clientWidth);
+        y = Math.floor(Math.random() * gui.gameScreen.clientHeight);
+        
+        if (x < 50 && x < gui.gameScreen.clientWidth)
+            x = Math.floor(Math.random() * gui.gameScreen.clientWidth);
+    
+        if (y < 50 && y < gui.gameScreen.clientHeight)
+            y = Math.floor(Math.random() * gui.gameScreen.clientHeight);
+
+        this.x = x;
+        this.y = y;
     }
 }
 
@@ -91,9 +105,23 @@ class Player {
         gameScreenContext.fillRect(this.x, this.y, this.width, this.height);
         gameScreenContext.fillStyle = '#000';
     }
+
+    collidedWith(object) {
+        return (
+            this.x + this.width > object.x &&
+            object.x + object.width > this.x &&
+            this.y + this.height > object.y &&
+            object.y + object.height > this.y
+        );
+    }
 }
 
 const textsButton = ['start', 'restart', 'new game'];
+
+const startingPos = {
+    player: { x: 30, y: 500 },
+    token: { x: 90, y: 50 }
+};
 
 const gameData = {
     scoreCurrent: 0,
@@ -122,12 +150,20 @@ const gameData = {
 
 // New game screen frame
 function frame() {
-    updateGUI();
-    updateGameData();
-    clearFrame();
-    drawFrame();
+    if (gameData.running) {
+        updateGUI();
+        updateGameData();
+        clearFrame();
+        drawFrame();
 
-    window.requestAnimationFrame(frame);
+        window.requestAnimationFrame(frame);
+    }
+}
+
+// Edit overlay text and color
+function editOverlay(text, color) {
+    gui.overlay.innerHTML = text;
+    gui.overlay.style.color = color;
 }
 
 // Update game gui
@@ -141,8 +177,33 @@ function updateGUI() {
 function updateGameData() {
     getDeltaTime();
 
-    if (gameData.isPlayerMoving)
+    if (gameData.isPlayerMoving && gameData.running) {
         gameData.objects.player.update(gameData.deltaTime, gameData.playerMoveDirection);
+
+        if (gameData.objects.player.collidedWith(gameData.objects.token)) {
+            let tokenSpawnAvoidObjects = [gameData.objects.player];
+
+            for (let i = 0; i < gameData.objects.obstacles.length; i++)
+                tokenSpawnAvoidObjects.push(gameData.objects.obstacles[i]);
+
+            gameData.objects.token.spawnRandom(tokenSpawnAvoidObjects);
+
+            gameData.scoreCurrent += 1;
+
+            if (gameData.scorePersonalBest < gameData.scoreCurrent)
+                gameData.scorePersonalBest = gameData.scoreCurrent;
+        } else if (
+            gameData.objects.player.collidedWith(gameData.objects.obstacles[0]) ||
+            gameData.objects.player.collidedWith(gameData.objects.obstacles[1]) ||
+            gameData.objects.player.collidedWith(gameData.objects.obstacles[2]) ||
+            gameData.objects.player.collidedWith({x:0, y:0, width:10, height:gui.gameScreen.clientHeight}) ||
+            gameData.objects.player.collidedWith({x:0, y:0, width:gui.gameScreen.clientHeight, height:10}) ||
+            gameData.objects.player.collidedWith({x:gui.gameScreen.clientWidth - 10, y:0, width: 10, height: gui.gameScreen.clientHeight}) ||
+            gameData.objects.player.collidedWith({x:0, y:gui.gameScreen.clientHeight - 10, width: gui.gameScreen.clientWidth, height: 10})
+        ) {
+            playerLost();
+        }
+    }
 }
 
 // Calculate and set delta time
@@ -157,12 +218,14 @@ function getDeltaTime() {
 
 // Draw game screen frame
 function drawFrame() {
-    gameData.objects.wall.draw();
-    gameData.objects.token.draw();
-    gameData.objects.player.draw();
-
-    for (let o of gameData.objects.obstacles) {
-        o.draw();
+    if (gameData.running) {
+        gameData.objects.wall.draw();
+        gameData.objects.token.draw();
+        gameData.objects.player.draw();
+    
+        for (let o of gameData.objects.obstacles) {
+            o.draw();
+        }
     }
 }
 
@@ -173,68 +236,80 @@ function clearFrame() {
 
 // Start running game
 function startGame() {
-    gameData.buttonText = textsButton[1];
-    gameData.running = true;
-
-    frame();
+    if (gameData.running) {
+        gameData.buttonText = textsButton[1];
+    
+        frame();
+    }
 }
 
+// If player is being drawn horizontally - draw vertically, and vice versa
+function changePlayerAxisDraw() {
+    let tempPlayerWidth = 0;
+    tempPlayerWidth = gameData.objects.player.width;
+    gameData.objects.player.width = gameData.objects.player.height;
+    gameData.objects.player.height = tempPlayerWidth;
+}
+
+// Move player
+function playerMove(axis, direction) {
+    if (axis === 'vertical' && gameData.objects.player.width > gameData.objects.player.height)
+        changePlayerAxisDraw();
+    else if (axis === 'horizontal' && gameData.objects.player.width < gameData.objects.player.height)
+        changePlayerAxisDraw();
+
+    gameData.isPlayerMoving = true;
+    gameData.playerMoveDirection = direction;
+}
+
+function playerLost() {
+    gameData.running = false;
+    editOverlay('GAME OVER', '#e74c3c');
+    gui.button.innerHTML = textsButton[2];
+}
+
+editOverlay('Movement: WASD / Arrow keys', '#bdc3c7');
 updateGUI();
 
 gui.button.addEventListener('click', function() {
     if (this.innerHTML === 'start') {
+        gameData.objects.player.speed = 300;
+
+        gameData.running = true;
+        gui.overlay.innerHTML = '';
+        startGame();
+        updateGUI();
+    } else if (this.innerHTML === 'new game' || this.innerHTML === 'restart') {
+        gameData.objects.player.speed = 300;
+
+        gameData.scoreCurrent = 0;
+        gameData.objects.player.x = startingPos.player.x;
+        gameData.objects.player.y = startingPos.player.y;
+        gameData.objects.token.x = startingPos.token.x;
+        gameData.objects.token.y = startingPos.token.y;
+
+        gameData.running = true;
+        gui.overlay.innerHTML = '';
         startGame();
         updateGUI();
     }
 });
 
 document.body.addEventListener('keydown', (e) => {
-    let tempPlayerWidth = 0;
-
-    switch (e.key) {
-        case 'w':
-            if (gameData.objects.player.width > gameData.objects.player.height) {
-                tempPlayerWidth = gameData.objects.player.width;
-                gameData.objects.player.width = gameData.objects.player.height;
-                gameData.objects.player.height = tempPlayerWidth;
-            }
-
-            gameData.isPlayerMoving = true;
-            gameData.playerMoveDirection = 'forwards';
-            break;
-        case 'a':
-            if (gameData.objects.player.width < gameData.objects.player.height) {
-                tempPlayerWidth = gameData.objects.player.width;
-                gameData.objects.player.width = gameData.objects.player.height;
-                gameData.objects.player.height = tempPlayerWidth;
-            }
-            
-            gameData.isPlayerMoving = true;
-            gameData.playerMoveDirection = 'left';
-            break;
-        case 's':
-            if (gameData.objects.player.width > gameData.objects.player.height) {
-                tempPlayerWidth = gameData.objects.player.width;
-                gameData.objects.player.width = gameData.objects.player.height;
-                gameData.objects.player.height = tempPlayerWidth;
-            }
-            
-            gameData.isPlayerMoving = true;
-            gameData.playerMoveDirection = 'backwards';
-            break;
-        case 'd':
-            if (gameData.objects.player.width < gameData.objects.player.height) {
-                tempPlayerWidth = gameData.objects.player.width;
-                gameData.objects.player.width = gameData.objects.player.height;
-                gameData.objects.player.height = tempPlayerWidth;
-            }
-            
-            gameData.isPlayerMoving = true;
-            gameData.playerMoveDirection = 'right';
-            break;
+    if (gameData.running) {
+        if (e.key === 'w' || e.key === 'ArrowUp')
+            playerMove('vertical', 'forwards');
+        else if (e.key === 'a' || e.key === 'ArrowLeft')
+            playerMove('horizontal', 'left');
+        else if (e.key === 's' || e.key === 'ArrowDown')
+            playerMove('vertical', 'backwards');
+        else if (e.key === 'd' || e.key === 'ArrowRight')
+            playerMove('horizontal', 'right');
     }
 });
 
 document.body.addEventListener('keyup', () => {
     gameData.isPlayerMoving = false;
 });
+
+setInterval(() => { gameData.objects.player.speed += 5; }, 1000);
